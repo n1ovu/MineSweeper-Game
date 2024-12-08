77b98fb1bdd0d4ccc16d0cd5cc5de8837ea385bb
n1ovu <n1ovu@outlook.com>
2024-12-08 00:39:22 -0500

    Second commit
    Finished createBoard.

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Display or UI
 import { createBoard } from "./minesweeper.js"
 
-const BOARD_SIZE = 2
-const NUMBER_OF_MINES = 2
+const BOARD_SIZE = 10
+const NUMBER_OF_MINES = 10
 
+// 1. Populate a board with tiles/mines
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
-
 const boardElement = document.querySelector(".board")
-boardElement.style.setProperty("--size", BOARD_SIZE)
+const minesLeftText = document.querySelector("[data-mine-count]")
+
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.element)
   })
 })
-// 1. Populate a board with tiles/mines
-
+boardElement.style.setProperty("--size", BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
 // 2. Left click on tile
 // a. Reveal tile
 
